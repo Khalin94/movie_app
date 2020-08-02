c1ca4ae355fb1b6c0a985412767fe4e307f466bc
Khalin <mpzx1102@gmail.com>
2020-08-03 02:08:42 +0900

    about component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import First from "./First";
+
+/* app 안에서 컴포넌트를 만들어서 사용하는 방법!
+function First(){
+  return <h3>first Component!</h3>
+}
+*/
+
+const order = [
+  {
+    name: "first",
+    image:
+      "https://images.unsplash.com/photo-1570521070694-ebff520095bb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80",
+  },
+  {
+    name: "second",
+    image:
+      "https://images.unsplash.com/photo-1560773766-0bd79c039083?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80",
+  },
+  {
+    name: "third",
+    image:
+      "https://images.unsplash.com/photo-1556917452-890eed890648?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+  },
+  {
+    name: "fourth",
+    image:
+      "https://images.unsplash.com/photo-1523286877159-d9636545890c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80",
+  },
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello</h1>
+      {/* <First
+        abc="test String abc"
+        trueOrFalse={true}
+        someArr={["abc", 1, 2, 3, 4, 5]}
+        order={"second"}
+      />
+      <First order={"third"} />
+      <First order={"fourth"} /> */}
+      {order.map((number) => (
+        <First name={number.name} picture={number.image} />
+      ))}
     </div>
   );
 }
---
+++ src/First.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function First(props) {
+  //function First({order}){  props 내부에서 값을 읽어오는 방법 {order}로 바로 읽어올수 있다.
+  console.log(props);
+  /*
+  props 출력 결과
+  abc: "test String abc"
+  someArr: (6) ["abc", 1, 2, 3, 4, 5]
+  trueOrFalse: true
+  */
+  return (
+    <h3>
+      {props.name} Component! {props.image}
+    </h3>
+  );
+}
+
+export default First;
